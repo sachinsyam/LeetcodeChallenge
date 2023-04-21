77e30cd3768f30c0ae12264e329ad70e859818b3
Sachin <sachinsyam@gmail.com>
2023-04-21 10:52:06 +0530

    solution in one line using lambda

--- src/Apr21.java
+++ src/Apr21.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Apr21 {
     public static void main(String[] args) {
         int [] arr = new int[]{1,2,3,4,5};
-        solution1(arr);
-        solution2(arr);
+//        solution1(arr);
+//        solution2(arr);
+//        int steps = minStepsToMakeAllEqual(arr);
+//        System.out.println(steps);
+        System.out.println(minStepsLambda(arr, arr[arr.length/2]));
+
     }
     static void solution1(int[] arr){
         int max = arr[arr.length-1];
@@ -23,4 +29,20 @@ public class Apr21 {
         }
         System.out.println(count);
     }
+    //finds minimum steps to make all elements equal by making all elements to mid element.
+    public static int minStepsToMakeAllEqual(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int median = n % 2 == 0 ? (nums[n/2-1] + nums[n/2]) / 2 : nums[n/2];
+        int steps = 0;
+        for (int num : nums) {
+            steps += Math.abs(num - median);
+        }
+        return steps;
+    }
+
+    public static int minStepsLambda(int [] nums, int mid){
+        return Arrays.stream(nums).map(n -> Math.abs(n - mid)).sum();
+    }
+
 }
