8a764acd5d87d560f6731ca879c5bd59b2cc6585
Sachin <sachinsyam@gmail.com>
2023-05-01 10:09:32 +0530

    may1     //find the missing number in a series given the actual lenfth

---
+++ src/May1.java
@@ -0,0 +1,15 @@
+public class May1 {
+    //find the missing number in a series given the actual lenfth
+    public static void main(String[] args) {
+        int arr[] = new int[]{0,1,3};
+        int n = 4;
+        int arrSum = 0;
+        int actualSum = 0;
+        for (int i = 0; i <n-1 ; i++) {
+            actualSum += i;
+            arrSum += arr[i];
+        }
+        actualSum += n-1;
+        System.out.println(actualSum-arrSum);
+    }
+}
